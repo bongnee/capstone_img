30e425a15e16668cedd1041478a1216d6b48c68d
thư nguyễn <nnmthu08@gmail.com>
2024-02-03 21:58:09 +0700

    p1

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -3,3 +3,73 @@ import { PrismaClient } from '@prisma/client';
 
 
 const prisma = new PrismaClient();
+
+const getUser = async (req, res) => {
+    let { id } = req.params;
+    let { token } = req.headers;
+    let isValidToken = checkToken(token);
+    console.log(isValidToken)
+    try {
+      let data = await prisma.users.findMany({
+        where: {
+          id: Number(id),
+        },
+      });
+  
+      if (data.length === 0) {
+        res.send("user id does not exits");
+      } else {
+        if (data.id == isValidToken.data.data.id) {
+
+          res.send(isValidToken.data);
+        } else {
+         
+          res.send(data);
+        }
+      }
+    } catch (error) {
+      res.send(`BE error ${error}`);
+    }
+  };
+
+  const getSavedImgdByUserId = async (req, res) => {
+    let { id } = req.params;
+  
+    try {
+      let data = await prisma.saved_imaged.findMany({
+        where: {
+          user_id: Number(id),
+        },
+      });
+  
+      if (data.length === 0) {
+        res.send("user id does not exits");
+      } else {
+        res.send(data);
+      }
+    } catch (error) {
+      res.send(`BE error ${error}`);
+    }
+  };
+
+  const getImgCreatedByUserId = async (req, res) => {
+    let { id } = req.params;
+  
+    try {
+      let data = await prisma.image.findMany({
+        where: {
+          user_id: Number(id),
+        },
+      });
+  
+      if (data.length === 0) {
+        res.send("user id does not exits");
+      } else {
+        res.send(data);
+      }
+    } catch (error) {
+      res.send(`BE error ${error}`);
+    }
+  };
+
+  export {getUser,getSavedImgdByUserId, getImgCreatedByUserId}
--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,1 +1,12 @@
 import express from "express";
+import { getImgCreatedByUserId, getSavedImgdByUserId, getUser } from "../controllers/userController.js";
+import { verifyToken } from "../config/jwt.js";
+
+
+
+const userRoutes = express.Router();
+userRoutes.get("/get-user", getUser);
+userRoutes.get("/get-saved-img-by-userid/:id",verifyToken, getSavedImgdByUserId);
+userRoutes.get("/get-img-created-by-userid/:id",verifyToken, getImgCreatedByUserId);
+
+export default  userRoutes;
